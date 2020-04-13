13f0b39d25638f09e490621637ef04853cd0dce6
thiagofd <thiago.fdornelles@gmail.com>
2020-04-13 10:14:06 -0300

    Updates

---
+++ precache-manifest.60f5af8190dddd013fd8c6c9ba7eb1e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "864565e763226d1401728b35919c60dc",
+    "url": "/To-Do-CommitJr/index.html"
+  },
+  {
+    "revision": "1332a9b04981d59fd43a",
+    "url": "/To-Do-CommitJr/static/css/main.db246747.chunk.css"
+  },
+  {
+    "revision": "a24bbd68827f4fe46a32",
+    "url": "/To-Do-CommitJr/static/js/2.746f8aa6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/To-Do-CommitJr/static/js/2.746f8aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1332a9b04981d59fd43a",
+    "url": "/To-Do-CommitJr/static/js/main.265b1cd4.chunk.js"
+  },
+  {
+    "revision": "d72b4271d92aa4ad287c",
+    "url": "/To-Do-CommitJr/static/js/runtime-main.a3e7961e.js"
+  }
+]);
